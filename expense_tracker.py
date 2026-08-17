fb8a4de9406a46e91f8cc0deb42e967664ed6030
aashisingh15 <aashi.possible@gmail.com>
2026-08-17 22:50:41 +0530

    Implement basic expense tracker functionality
    This script allows users to track their expenses by entering categories and amounts. Users can add expenses or view their total expenses.

---
+++ expense_tracker.py
@@ -0,0 +1,21 @@
+print("Hello this is your free expense tracker to continue we please enter your name")
+name=input("Enter your name: ")
+print("Hello",name)
+expenses={}
+choice = 1
+while choice == 1:
+    x = int(input("Enter 1 to add expense or 2 to view expense: "))
+    if x == 1:
+        number = int(input("How many expenses do you want to enter? "))
+        for i in range(number):
+                category = input("Enter category: ")
+                amount = int(input("Enter expense: "))
+                expenses[category] = amount
+            
+    elif x == 2:
+       print(expenses)
+       choice = int(input("Enter 1 to continue or 2 to stop: "))
+    else:
+        print("Error")
+print(expenses)
+print("Your total expense is:", sum(expenses.values())) 
